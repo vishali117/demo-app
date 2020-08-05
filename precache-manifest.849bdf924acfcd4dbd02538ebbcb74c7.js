df719285a575cb6dc08d220c5104e23ee0a5e32c
vishali123 <vishalisaraj117@gmail.com>
2020-08-05 13:21:48 +0530

    Updates

---
+++ precache-manifest.849bdf924acfcd4dbd02538ebbcb74c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b963bd5cdf592efb22b5be8feff3be",
+    "url": "/react-todo-list-app/index.html"
+  },
+  {
+    "revision": "e6e841093516c1f4de89",
+    "url": "/react-todo-list-app/static/css/main.64584ac1.chunk.css"
+  },
+  {
+    "revision": "b23c77f68e23ec6e62b5",
+    "url": "/react-todo-list-app/static/js/2.803be644.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list-app/static/js/2.803be644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e841093516c1f4de89",
+    "url": "/react-todo-list-app/static/js/main.73308d91.chunk.js"
+  },
+  {
+    "revision": "7ef5e211723116993f67",
+    "url": "/react-todo-list-app/static/js/runtime-main.ffab1261.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-list-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
